20c66e3083d6eadad271eb8cf975d8054b803eeb
vishalchinna <vishalpvs225@gmail.com>
2023-05-31 07:36:29 +0000

    1testCase F

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,33 +1,58 @@
 import {Component} from 'react'
 
-import suggestionList from '../SuggestionItem'
+import SuggestionItem from '../SuggestionItem'
 import './index.css'
 
 class GoogleSuggestions extends Component {
+  state = {inputSearch: ''}
+
+  refreshSuggestion = event => {
+    this.setState({inputSearch: event.target.value})
+  }
+
+  selectingList = suggestion => {
+    this.setState({inputSearch: suggestion})
+  }
+
   render() {
+    const {inputSearch} = this.state
     const {suggestionsList} = this.props
-    console.log(suggestionsList)
+    const filteredData = suggestionsList.filter(each =>
+      each.suggestion.toLowerCase().includes(inputSearch.toLowerCase()),
+    )
 
     return (
       <div className="bg-container">
         <div className="box-container">
           <img
             src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
-            alt="Google"
+            alt="google logo"
             className="logo"
           />
           <div className="search-box">
             <div className="search-bar">
               <img
                 src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
-                alt="search"
+                alt="search icon"
                 className="search"
               />
-              <input type="search" className="input" />
+              <input
+                type="search"
+                className="input"
+                placeholder="search google"
+                onChange={this.refreshSuggestion}
+                value={inputSearch}
+              />
             </div>
-            {suggestionsList.map(each => (
-              <suggestionList key={each.id} list={each} />
-            ))}
+            <ul>
+              {filteredData.map(each => (
+                <SuggestionItem
+                  key={each.id}
+                  selectingList={this.selectingList}
+                  dataList={each}
+                />
+              ))}
+            </ul>
           </div>
         </div>
       </div>
--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,10 +1,23 @@
 import './index.css'
 
-const suggestionList = props => {
-  const {list, key} = props
-  const {suggestion} = list
-  console.log(suggestion)
-  return <p className="para">{suggestion}</p>
+const SuggestionItem = props => {
+  const {dataList, selectingList} = props
+  const {suggestion} = dataList
+
+  const selectTheItem = () => {
+    selectingList(suggestion)
+  }
+  return (
+    <div className="searchList">
+      <li className="para">{suggestion}</li>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        alt="arrow"
+        className="arrow"
+        onClick={selectTheItem}
+      />
+    </div>
+  )
 }
 
-export default suggestionList
+export default SuggestionItem
